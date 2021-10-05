486cbcd167e763451e40dd2dbc69cfdb16aab0eb
dawerqut <32057303+dawerqut@users.noreply.github.com>
2021-10-05 18:07:46 +0300

    исправление замечаний

--- src/main/java/ru/mipt/bit/platformer/Direction.java
+++ src/main/java/ru/mipt/bit/platformer/Direction.java
@@ -1,32 +1,8 @@
 package ru.mipt.bit.platformer;
 
-import com.badlogic.gdx.Gdx;
-
-import static com.badlogic.gdx.Input.Keys.*;
-import static com.badlogic.gdx.Input.Keys.D;
-
 enum Direction {
     Up,
     Down,
     Left,
-    Right,
-    Default;
-
-    Direction getDirectionFromKey() {
-        if (Gdx.input.isKeyPressed(UP) || Gdx.input.isKeyPressed(W)) {
-            return Up;
-        }
-        else if (Gdx.input.isKeyPressed(LEFT) || Gdx.input.isKeyPressed(A)) {
-            return Left;
-        }
-        else if (Gdx.input.isKeyPressed(DOWN) || Gdx.input.isKeyPressed(S)) {
-            return Down;
-        }
-        else if (Gdx.input.isKeyPressed(RIGHT) || Gdx.input.isKeyPressed(D)) {
-            return Right;
-        }
-        else {
-            return Default;
-        }
-    }
+    Right
 }
