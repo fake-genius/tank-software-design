8e392bd8402ede15f1e7e39216515168b0158faa
dawerqut <32057303+dawerqut@users.noreply.github.com>
2021-10-20 00:13:00 +0300

    игрок теперь танк

--- src/main/java/ru/mipt/bit/platformer/gameobjects/GameObjectType.java
+++ src/main/java/ru/mipt/bit/platformer/gameobjects/GameObjectType.java
@@ -1,12 +1,11 @@
 package ru.mipt.bit.platformer.gameobjects;
 
 import com.badlogic.gdx.math.GridPoint2;
-import com.badlogic.gdx.scenes.scene2d.ui.Tree;
 
 public enum GameObjectType {
     PLAYER {
         public GameObject getObject(GridPoint2 coords) {
-            return new Player(coords);
+            return new Tank(coords);
         }
     },
     TREE {
