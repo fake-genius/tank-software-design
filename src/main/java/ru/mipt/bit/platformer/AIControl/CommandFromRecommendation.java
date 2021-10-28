70829c3760f7b87b993e435cd1384bcc2975e8e0
dawerqut <32057303+dawerqut@users.noreply.github.com>
2021-10-29 02:20:08 +0300

    ai adapter

---
+++ src/main/java/ru/mipt/bit/platformer/AIControl/CommandFromRecommendation.java
@@ -0,0 +1,23 @@
+package ru.mipt.bit.platformer.AIControl;
+
+import org.awesome.ai.Action;
+import org.awesome.ai.Recommendation;
+import org.awesome.ai.state.movable.Orientation;
+import ru.mipt.bit.platformer.control.*;
+import ru.mipt.bit.platformer.gameobjects.Tank;
+
+public class CommandFromRecommendation {
+    Command getCommand(Recommendation recommendation) {
+        Tank actor = (Tank) recommendation.getActor().getSource();
+        Action action = recommendation.getAction();
+        switch(action) {
+            case MoveEast:
+                return new MoveRightCommand(actor);
+            case MoveWest:
+                return new MoveLeftCommand(actor);
+            case MoveNorth:
+                return new MoveUpCommand(actor);
+        }
+        return new MoveDownCommand(actor);
+    }
+}
---
+++ src/main/java/ru/mipt/bit/platformer/AIControl/TankAIController.java
@@ -0,0 +1,41 @@
+package ru.mipt.bit.platformer.AIControl;
+
+import org.awesome.ai.AI;
+import org.awesome.ai.Recommendation;
+import org.awesome.ai.state.GameState;
+import ru.mipt.bit.platformer.AIControl.creators.GameStateCreator;
+import ru.mipt.bit.platformer.control.Command;
+import ru.mipt.bit.platformer.gameobjects.Tank;
+import ru.mipt.bit.platformer.gameobjects.TreeObstacle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TankAIController {
+    private final AI ai;
+    private final GameState gameState;
+    private final ArrayList<Command> tanksCommands;
+
+    public TankAIController(AI ai, Tank playerTank, ArrayList<TreeObstacle> treeObstacles, ArrayList<Tank> tanks, int levelWidth, int levelHeight) {
+        this.ai = ai;
+        GameStateCreator gameStateCreator = new GameStateCreator();
+        this.gameState = gameStateCreator.createGameState(playerTank, treeObstacles, tanks, levelWidth, levelHeight);
+        this.tanksCommands = new ArrayList<>();
+    }
+
+    public void executeCommands() {
+        recommendCommands();
+        for (Command command : tanksCommands) {
+            command.execute();
+        }
+    }
+
+    public void recommendCommands() {
+        List<Recommendation> recommendations = ai.recommend(gameState);
+        CommandFromRecommendation commandFromRecommendation = new CommandFromRecommendation();
+        for (Recommendation rec : recommendations) {
+            tanksCommands.add(commandFromRecommendation.getCommand(rec));
+        }
+    }
+
+}
